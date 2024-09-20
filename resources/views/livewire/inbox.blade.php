e3d9106c93649415306de142fc370ce489c069bb
Kevttv <kevttv29@gmail.com>
2024-09-19 20:53:07 -0500

    finish view for chat

--- resources/views/livewire/inbox.blade.php
+++ resources/views/livewire/inbox.blade.php
@@ -123,6 +123,15 @@
                                     </span>
 
                                     <input type="text" class="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-400 pl-12 bg-gray-100 dark:bg-gray-800 rounded-full py-3 pr-5">
+
+                                    <div class="ml-5">
+                                        <button class="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-white bg-indigo-800 hover:bg-indigo-600 focus:outline-none ">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                                            </svg>
+
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
