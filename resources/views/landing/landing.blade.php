5ec20623069866f827c07b6c15d7c293dc510174
Kevttv <kevttv29@gmail.com>
2024-09-26 10:15:36 -0500

    change in HTM structure

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -23,25 +23,34 @@
 
     </header>
 
-    <main>
-        <section class="p-2 space-y-4">
-            <div class="">
-                <img class="h-52 w-full rounded-xl" src="{{ asset('images/main_image.webp') }}"
-                    alt="Descripción de la imagen">
-            </div>
+    <main class="p-4">
+        <section class="space-y-6">
+            <article class="rounded-xl overflow-hidden">
+                <img class="h-52 w-full object-cover" src="{{ asset('images/main_image.webp') }}" alt="Descripción de la imagen">
+            </article>
 
-            <div class="flex justify-between items-center space-x-4">
+            <article class="flex flex-row justify-between items-center space-x-4">
                 <div class="flex-1">
-                    <h2 class="text-rosado text-2xl font-arista">
+                    <h2 class="text-rosado text-3xl md:text-4xl font-arista leading-tight">
                         Eleva el<br>
                         potencial de tu<br>
-                        <strong class="text-5xl">WhatsApp</strong>
+                        <strong class="text-5xl md:text-6xl">WhatsApp</strong>
                     </h2>
                 </div>
-                <div class="">
-                    <img class="h-32 transform scale-x-[-1]" src="{{ asset('images/celular.webp') }}" alt="">
-                </div>
-            </div>
+                <figure class="flex-shrink-0">
+                    <img class="h-32 md:h-44 transform scale-x-[-1]" src="{{ asset('images/celular.webp') }}" alt="Imagen de un celular">
+                </figure>
+            </article>
+
+
+            <article>
+                <div class="text-gray-800 text-xs leading-relaxed text-center">
+                    <p class="">Optimiza tus mensajes masivos en WhatsApp con un <span class="font-bold">sistema multiagente.</span></p>
+                    <p>Nuestra app permite que <span class="font-bold">varios agentes gestionen y envíen mensajes simultáneamente</span>, mejorando la eficiencia y ahorrando tiempo. Ideal para <span class="font-bold">empresas y equipos que buscan mejorar su comunicación y llegar a más personas rápidamente.</span></p>
+                    <p>Incluye funciones avanzadas como listas personalizadas, mensajes automáticos y seguimiento en tiempo real.</p>
+                    </div>
+
+            </article>
         </section>
     </main>
 </body>
