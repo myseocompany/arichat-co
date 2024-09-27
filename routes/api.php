dbb7bbc6202ff892af1f8bbfe6735c178c578415
Nicolás Navarro <nicolas@myseocompany.co>
2024-09-27 08:24:13 -0500

    API

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\WAToolBoxController;
+use App\Http\Controllers\API\WAToolBoxController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
