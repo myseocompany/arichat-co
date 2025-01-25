8ac844d23a2c786022acf23fe84178260464ae75
myseocompany <nicolas@myseocompany.co>
2025-01-24 23:34:23 -0500

    eliminar aritest

--- database/seeders/LeadSeeder.php
+++ database/seeders/LeadSeeder.php
@@ -45,5 +45,23 @@ class LeadSeeder extends Seeder
             'updated_at' => Carbon::now(),
         ]);
 
+        $leads = [];
+        $now = Carbon::now();
+                // Crear 40 leads asignados a usuarios y equipos válidos
+                for ($i = 1; $i <= 120; $i++) {
+                    $leads[] = [
+                        'user_id' => $i % 3 + 1, // Asignar a usuarios con IDs 1, 2, o 3
+                        'team_id' => $i % 3 + 1, // Asignar a equipos con IDs 1, 2, o 3
+                        'name' => "Lead $i", // Nombre dinámico del lead
+                        'email' => "lead$i@company.com", // Email dinámico del lead
+                        'phone' => '57300' . rand(1000000, 9999999), // Generar un teléfono aleatorio
+                        'created_at' => $now,
+                        'updated_at' => $now,
+                    ];
+                }
+        
+                // Insertar los leads en la base de datos
+                DB::table('leads')->insert($leads);
+
     }
 }
